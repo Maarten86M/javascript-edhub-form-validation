8ce38bd3ea9fcd0d16395a605577000bb9cfc959
Maarten86M <maartenmoelands@gmail.com>
2021-04-22 15:41:03 +0200

    tot zover gaat ht goed

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+const inputField = document.getElementById('username');
+const warningField = document.getElementById('form-warning');
+
+function emailCheck(){
+    console.log("emailcheck")
+}
+
+inputField.addEventListener('keyup', emailCheck);
