4611a72fcb18fcd9722543e89f2bf80b3ca1b790
Maarten86M <maartenmoelands@gmail.com>
2021-04-22 15:58:58 +0200

    oefening afgerond

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 
-
+// sla de referentie op naar de input
 const inputField = document.getElementById('username');
-const warningField = document.getElementById('form-warning');
+const passwordField = document.getElementById('password');
 
-function emailCheck(){
-    console.log("emailcheck")
+// sla de referentie op naar de warning message.
+const warningField = document.getElementById('form-warning');
+const warningFieldPass = document.getElementById('form-warning-pass');
+
+// voeg eventlisteners toe op de inputvelden.
+inputField.addEventListener('keyup', emailCheck);
+passwordField.addEventListener("keyup", passwordCheck);
+
+
+// functie voor de gebruikersnaam.
+
+function emailCheck(e){
+
+    if (e.target.value.includes("@")){
+        warningField.textContent = "gebruikersnaam mag geen @ bevatten";
+    } else {
+        warningField.textContent = '';
+    }
 }
 
-inputField.addEventListener('keyup', emailCheck);
+// functie voor de wachtwoordcheck.
+
+function passwordCheck(e){
+    if (e.target.value === '' || e.target.value.length > 6) {
+        warningFieldPass.textContent = '';
+    }else {
+        warningFieldPass.textContent = "Wachtwoord is te kort, gebruik minimaal 6 tekens";
+    }
+}
